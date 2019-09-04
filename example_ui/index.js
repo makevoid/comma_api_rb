08899a1c06abc4ea75881b569565c7ccd02aae66
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-09-04 07:25:41 +0100

    add chart

--- example_ui/index.js
+++ example_ui/index.js
@@ -1,8 +1,23 @@
 const chartOptions = {
 	type: 'line',
-	data: data,
 	options: options,
 }
 
-const gps = new Chart('lat-lng', cartOptions)
-const compass = new Chart('lat-lng', cartOptions)
+// html
+// ---
+// #gps1.gps1.gps1Chart.chart1
+// #compass.compass.compassChart.chart2
+
+const merge = (obj1, obj2) =>
+	Object.assign(obj1, obj2)
+
+const data1 = [1, 2, 3]
+const data2 = [2, 3, 4]
+
+let data
+
+data = { data: data1 }
+const gps			= new Chart( 'gps1', 		merge(cartOptions, data1) )
+
+data = { data: data2 }
+const compass	= new Chart( 'compass', merge(cartOptions, data2) )
