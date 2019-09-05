26696a504437b753ada71bc7ab742e363f16c469
makevoid <makevoid@gmail.com>
2019-09-05 06:37:00 +0100

    update SSE example for chart.js based dashboard - data api

--- examples/default_example.rb
+++ examples/default_example.rb
@@ -13,16 +13,84 @@ puts "me: #{me}\n\n"
 
 # ---
 
-# deviceLoc = CommaAPI.deviceDefaultLocation()
-# puts "device location: #{deviceLoc}\n\n"
+deviceLoc = CommaAPI.deviceDefaultLocation()
+puts "device location: #{deviceLoc}\n\n"
 # # lat lng time speed bearing
 # # note - takes long ? because device is offline?
 
 
-stats = CommaAPI.deviceDefaultStats()
-puts "device stats: #{stats}\n\n"
+puts "Example loop and update via SSE" # - https://github.com/appliedblockchain/roda-react-sse
+
+# Example where I use roda and SSE events ( needs full roda app - code from https://github.com/appliedblockchain/roda-react-sse )
+
+module FormatStream
+
+  SSE_IDS = {}
+
+  Format = -> (stream, hash) {
+    out = ""
+    id = SSE_IDS[:stream] || 0 # we need an auto-increment id for the sse to work
+    # this is the (simple) output format - id: ID \n data: [JSON] \n\n
+    out << "id: #{id}\n"
+    out << "data: #{JSONDump.(hash)} \n\n" # json data
+    SSE_IDS[:stream] = id
+    out
+  }
+
+  # private
+
+  JSONDump = -> (hash) {
+    Oj.dump(hash, mode: :compat)
+  }
+
+end
+
+Log = -> (msg) {
+  puts msg
+}
+
+FetchLocation = -> {
+  deviceLoc = CommaAPI.deviceDefaultLocation()
+  Log.("new device location: #{deviceLoc}\n\n")
+  deviceLoc
+}
+
+r.on "updates" do
+  response['Access-Control-Allow-Origin'] = CONFIG[:host]
+  response['Content-Type'] = 'text/event-stream'
+  stream do |out|
+    while true do
+      data = FetchLocation.()
+      out << FormatStream::Format.(:update_location, data)
+      sleep 3
+    end
+  end
+end
+
+# --------------------
+
+# JS
+
+JS = `
+const source = new EventSource('/subscribe')
+
+const logData = function(data) {
+  log.innerText += '\n' + data
+}
+
+source.addEventListener('message', (event) => {
+  logData(event.data)
+}, false)
+`
+
+
+
+# --------------------
+
+# stats = CommaAPI.deviceDefaultStats()
+# puts "device stats: #{stats}\n\n"
 # distance, minutes, routes
-# total stats: miles driven, minutes, number of sessions 
+# total stats: miles driven, minutes, number of sessions
 
 
 
