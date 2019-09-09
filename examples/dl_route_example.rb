a777cbd13dfe62f5b57d16b41af3ce2e4adc7457
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-09-09 12:34:18 +0100

    add more examples on route ifno

--- examples/dl_route_example.rb
+++ examples/dl_route_example.rb
@@ -2,22 +2,37 @@
 
 require_relative '../api_client'
 
-me = CommaAPI.me()
-puts "me: #{me}\n\n"
+route_id = "........"
+route_id = CGI.escape route_id
+
+routeFiles = CommaAPI.routeFiles route_name: route_id
+puts "routeFiles: #{routeFiles}\n\n"
+
+routeFiles.fetch("cameras").each_with_index do |camera, idx|
+  url  = camera
+  uri  = URI url
+  req  = Net::HTTP::Get.new uri.request_uri
+  req["Authorization"] = "JWT #{::JWT_TOKEN}"
+  resp = CommaAPI.http(uri: uri).request req
+  return RPCError404.new if resp.code == "404"
+
+  data = resp.body
+  idx = "%02d" % idx
+  File.open("video-#{idx}.hevc", "wb") do |file|
+    file.write data
+  end
+end
 
 
-# route_id = "xxx|2019-xxx"
-#
-# route_id = CGI.escape route_id
-#
-#
+
+
+#  ---
+
+# me = CommaAPI.me()
+# puts "me: #{me}\n\n"
+
 # segments = CommaAPI.deviceDefaultSegments
 # puts "segments: #{segments}\n\n"
-#
-#
+
 # route = CommaAPI.route route_name: route_id
 # puts "route: #{route}\n\n"
-#
-#
-# routeFiles = CommaAPI.routeFiles route_name: route_id
-# puts "routeFiles: #{routeFiles}\n\n"
---
+++ examples/route_info_example.rb
@@ -0,0 +1,24 @@
+# ruby dl_route.rb <route_id>
+
+require_relative '../api_client'
+
+me = CommaAPI.me()
+puts "me: #{me}\n\n"
+
+# choose ID and uncomment
+
+# route_id = "xxx|2019-xxx"
+#
+# route_id = CGI.escape route_id
+#
+#
+# segments = CommaAPI.deviceDefaultSegments
+# puts "segments: #{segments}\n\n"
+#
+#
+# route = CommaAPI.route route_name: route_id
+# puts "route: #{route}\n\n"
+#
+#
+# routeFiles = CommaAPI.routeFiles route_name: route_id
+# puts "routeFiles: #{routeFiles}\n\n"
