846dc7b3a7ab9400b7a37fb17d9382cb64e7abf9
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-09-15 13:32:59 +0100

    rename example dir

---
+++ example/priv_example1.rb
@@ -0,0 +1,24 @@
+# ruby dl_route.rb <route_id>
+
+require_relative '../api_client'
+
+route_id = "e81d9375682aae7b|2019-07-20--14-36-41"
+route_id = CGI.escape route_id
+
+routeFiles = CommaAPI.routeFiles route_name: route_id
+puts "routeFiles: #{routeFiles}\n\n"
+
+routeFiles.fetch("cameras").each_with_index do |camera, idx|
+  url  = camera
+  uri  = URI url
+  req  = Net::HTTP::Get.new uri.request_uri
+  req["Authorization"] = "JWT #{::JWT_TOKEN}"
+  resp = CommaAPI.http(uri: uri).request req
+  return RPCError404.new if resp.code == "404"
+
+  data = resp.body
+  idx = "%02d" % idx
+  File.open("video-#{idx}.hevc", "wb") do |file|
+    file.write data
+  end
+end
